77a0a8f2c922c072e042aa2df23ec32b70b9589e
ThePhaseCat <notepad.exe@indylaptop>
2022-04-24 13:30:13 -0600

    added moon dust

---
+++ src/main/java/net/mcreator/starbound/block/MoonDustBlock.java
@@ -0,0 +1,35 @@
+
+package net.mcreator.starbound.block;
+
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.FallingBlock;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.BlockPos;
+
+import java.util.List;
+import java.util.Collections;
+
+public class MoonDustBlock extends FallingBlock {
+	public MoonDustBlock() {
+		super(BlockBehaviour.Properties.of(Material.SAND).sound(SoundType.SAND).strength(1f, 10f));
+		setRegistryName("moon_dust");
+	}
+
+	@Override
+	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return 15;
+	}
+
+	@Override
+	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+		if (!dropsOriginal.isEmpty())
+			return dropsOriginal;
+		return Collections.singletonList(new ItemStack(this, 1));
+	}
+}
